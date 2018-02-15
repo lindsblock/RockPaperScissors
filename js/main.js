7f515ef340165b038e038139da91db39e8cbdd88
Lindsay Block <lindsay.block05@gmail.com>
2018-02-14 18:10:24 -0700

    working on layout

--- js/main.js
+++ js/main.js
@@ -1,16 +1,61 @@
+$(document).ready( function() {    
+  
+  
+var userChoice
+var computerChoice 
+var result 
+var options = ['rock', 'paper', 'scissors']
+
+
 function startGame(e) {
-  var usersChoice = e.target.id // the ID of whatever they clicked on 
-  console.log(usersChoice)   //lists computer's choice 
+  var userChoice = this.id // the ID of whatever they clicked on 
+     
   computerChoice()
-  checkWin(usersChoice)
+  checkWin()
   displayResult()
 }
 
-var choices = document.getElementsByClassName('choice')
-for (var i in choices) {
-  try {
-    choices[i].addEventListener('click', startGame)
-  } catch(err) {
-    //First load
+function computerChoice(){
+  var index = Math.floor(Math.random()* options.length) //yesterday we did *3 but .length makes more sense 
+  return options[index];
+}
+
+function checkWin(usersChoice, computerChoice) {
+  if(usersChoice === "rock"){
+    if(computerChoice === "scissors"){
+        console.log( "rock wins");
+      } else {
+        console.log( "paper wins"); 
+      }
+    
+  }
+
+  if(usersChoice === "paper"){
+    if(computerChoice === "rock") {
+        console.log( "paper wins");
+    } else {
+          console.log( "scissors wins");
+        }
+    
+  }
+
+  if(usersChoice === "scissors"){
+    if(computerChoice === "rock") {
+        console.log( "rock wins");
+    } else {
+        console.log("scissors wins"); 
+      }
+    
   }
 }
+
+function displayResult(){
+  console.log()
+}
+
+$('.choice').on('click', function(){
+  this.id 
+  console.log(this.id)
+  startGame(this.id)
+})
+});
